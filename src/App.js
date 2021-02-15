bb1d4f8bd8fe209a6cd351f14c93aa96c8bb02a5
Miguel Zegarra <42479401+AckerCoder@users.noreply.github.com>
2021-02-14 20:28:00 -0500

    Day 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
