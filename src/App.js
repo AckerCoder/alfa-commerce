5f8eed4ff4579f42e8501fa2d1fb1c539a79ee1e
Miguel Zegarra <42479401+AckerCoder@users.noreply.github.com>
2021-02-26 11:50:59 -0500

    ROUTER CHANGES

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const TopicList = (props) => {
+  console.log(props.path);
+  return(
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  )
+};
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL</h1>
+  </div>
+);
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage></HomePage>
-    </div>
+      <div>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/topics' component={TopicList} />
+        <Route path='/topics/:topicId' component={TopicDetail} />
+      </div>
   );
 }
 
