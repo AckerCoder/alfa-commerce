7c9cd707e4b19425ec579c76b2f0fe26baeb2e6f
Miguel <ackercoder@gmail.com>
2021-03-04 00:44:00 -0500

    Creating the sign in page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up-page.styles.scss'
+
+const SignInAndSignUpPage = () => {
+    return(
+        <div className='sign-in-adn-sign-up'>
+            <SignIn/>
+        </div>
+    )
+};
+
+export default SignInAndSignUpPage;
