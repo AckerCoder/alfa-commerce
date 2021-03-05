a0c8d18390a6d34148a4a097e20d6e36f53f46de
Miguel Zegarra <42479401+AckerCoder@users.noreply.github.com>
2021-03-04 21:48:23 -0500

    little changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component'
-import Header from './components/header/header.component.jsx'
+import Header from './components/header/header.component.jsx';
+import {auth} from './firebase/firebase.utils';
 
 function App() {
   return (
