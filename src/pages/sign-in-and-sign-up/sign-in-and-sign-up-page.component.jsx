21a711026567b0ccbeb80a05d5a23af3089d3405
Miguel <ackercoder@gmail.com>
2021-03-26 15:59:14 -0500

    implemented sign-up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 import './sign-in-and-sign-up-page.styles.scss'
 
 const SignInAndSignUpPage = () => {
     return(
-        <div className='sign-in-adn-sign-up'>
+        <div className='sign-in-and-sign-up'>
             <SignIn/>
+            <SignUp/>
         </div>
     )
 };
